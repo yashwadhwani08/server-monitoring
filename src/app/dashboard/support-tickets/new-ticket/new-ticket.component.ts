ae72d0e250661d337faa253f40a2c700b9af711d
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-28 14:30:15 +0530

    evidented the difference between ngInit and ngAfterViewInit, so if anything related to getting some data from template after App launch, use ngAfterViewInit

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -10,14 +16,23 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+export class NewTicketComponent implements AfterViewInit {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
 
-  private form = viewChild.required<ElementRef<HTMLFormElement>>('form')
+  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit(){
+    console.log('ONINIT');
+    console.log(this.form?.nativeElement);
+  }
+  ngAfterViewInit(): void {
+      console.log('AFTER VIEW INIT');
+      console.log(this.form?.nativeElement)
+  }
 
   onSubmit(title: string, ticketText: string) {
     console.log(title);
     console.log(ticketText);
-    this.form().nativeElement.reset();
+    this.form?.nativeElement.reset();
   }
 }
