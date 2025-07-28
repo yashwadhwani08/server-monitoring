376fd92acc8630de898f28d572fb93ad1a457b1d
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-28 10:40:56 +0530

    working with template variables to get the values for fields in form

--- src/app/dashboard/sever-status/sever-status.component.ts
+++ src/app/dashboard/sever-status/sever-status.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './sever-status.component.css',
 })
 export class SeverStatusComponent {
-  currentStatus:string = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  // private interval ?: NodeJS.Timeout;
+  private interval ?: ReturnType<typeof setInterval>;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
+  }
 }
--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-
-  onSubmit(){
-
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
   }
-
 }
