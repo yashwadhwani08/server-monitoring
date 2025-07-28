142cb7090a1de0c69d1ad45cf4ec58932bc9af5f
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-28 16:13:50 +0530

    showing submitted ticket data

--- src/app/dashboard/support-tickets/ticket/ticket.component.ts
+++ src/app/dashboard/support-tickets/ticket/ticket.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  detailsVisible = signal(false);
 
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update((wasVisible) => !wasVisible);
+  }
 }
